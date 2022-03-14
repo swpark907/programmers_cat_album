610cdf63b5a1121dbc17688d0cd5b17d715761f0
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-15 00:57:02 +0900

    update fetch url

---
+++ src/api.js
@@ -0,0 +1,17 @@
+const API_END_POINT = 'https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev';
+
+export const request = async (nodeId) => {
+    console.log('요청');
+    try{
+        const response = await fetch(`${API_END_POINT}/${nodeId}`)
+
+        if(!response.ok){
+            throw new Error('요청중 문제 발생')
+        }
+        
+        return await response.json();
+    } catch(e) {
+        throw new Error(e);
+    }
+}
+
