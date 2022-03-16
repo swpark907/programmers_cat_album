d6d3a4cda9e98aae96299821e7c1ac24a661cc3e
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-16 20:24:17 +0900

    update url 조건문

--- src/api.js
+++ src/api.js
@@ -3,12 +3,12 @@ const API_END_POINT = 'https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.c
 export const request = async (nodeId) => {
     console.log('요청');
     try{
-        const response = await fetch(`${API_END_POINT}/${nodeId}`)
+        const response = await fetch(`${API_END_POINT}/${nodeId? nodeId : ''}`)
 
         if(!response.ok){
             throw new Error('요청중 문제 발생')
         }
-        
+
         return await response.json();
     } catch(e) {
         throw new Error(e);
