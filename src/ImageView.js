2e1b79a411ddddf7761bd3db40bb76228e1a6378
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-16 20:23:15 +0900

    update export component

--- src/ImageView.js
+++ src/ImageView.js
@@ -1,7 +1,7 @@
 const IMAGE_PATH_PREFIX =
   "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
 
-function ImageView({ $app, initialState }) {
+export default function ImageView({ $app, initialState }) {
   // image.url
   this.state = initialState;
   this.$target = document.createElement("div");
