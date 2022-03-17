45c0e70176d166445ea725dadb9f5a09454a1a9f
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-18 02:18:55 +0900

    update render, click event

--- src/Nodes.js
+++ src/Nodes.js
@@ -1,26 +1,63 @@
-export default function Nodes ({$app, initialState, onClick}) {
-
+export default function Nodes({ $app, initialState, onClick, onBackClick }) {
   this.onClick = onClick;
+  this.onBackClick = onBackClick;
 
   this.state = initialState;
 
-  this.$target = document.createElement('ul');
-  this.$target.className = 'Nodes'
+  this.$target = document.createElement("ul");
+  this.$target.className = "Nodes";
   $app.appendChild(this.$target);
 
   this.setState = (nextState) => {
-      this.state(nextState);
-
-      this.render();
-  }
+    this.state = nextState;
+    this.render();
+  };
 
   this.render = () => {
-      this.$target.innerHTML = this.state.nodes.map((node) => (
-          `<li>
-              ${node.name}
+    const targetTemplate = this.state.nodes
+      .map((node) => {
+        const iconPath =
+          node.type === "DIRECTORY" ? "directory.png" : "file.png";
+
+        return `
+        <li class="Node" data-node-id=${node.id}>
+            <img src='assets/${iconPath}'
+            <div>${node.name}</div>
+          </li>`;
+      })
+      .join("");
+
+    this.$target.innerHTML = `
+      ${
+        !this.state.isRoot
+          ? `<li class="Node prev">
+          <img src="assets/prev.png">
           </li>`
-      ))
-  }
+          : ''
+      }
+      ${targetTemplate}
+    `;
+  };
+
+  this.$target.addEventListener("click", (event) => {
+    const $node = event.target.closest(".Node");
+
+    if ($node) {
+      const { nodeId } = $node.dataset;
+
+      if(!nodeId) {
+        this.onBackClick();
+        return;
+      }
+
+      const selectedNode = this.state.nodes.find((node) => node.id === nodeId);
+      console.log({ selectedNode });
+
+      if (selectedNode) {
+        this.onClick(selectedNode);
+      }
+    }
+  });
 
   this.render();
 }
